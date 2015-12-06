e6b70f857e8ab5ae3cff368b433d3bd49df611e4
Vnu <dearvnu@yahoo.in>
2015-12-06 12:02:53 -0800

    Initial Commit

---
+++ Tip Smart/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip Smart
+//
+//  Created by Vinu Charanya on 12/6/15.
+//  Copyright © 2015 vnu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
